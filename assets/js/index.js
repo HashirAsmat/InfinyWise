62bb8763a0b8da614f9798aa7ae08ae7fdf06182
HashirAsmat <103664858+HashirAsmat@users.noreply.github.com>
2023-09-03 15:50:52 +0500

    Bug FIxed , now no one can submit the form , if any of the required field is empty...

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,19 +8,23 @@ function sendMail(){
         phonenumber: document.getElementById("phonenumber").value,
         message: document.getElementById("message").value,
     }
-
-    const serviceID = "service_2k39r8u";
-    const templateID = "template_us8fs3m"
-    emailjs.send(serviceID,templateID,params).then(function (res){
-        document.getElementById('firstname').value = "";
-        document.getElementById("lastname").value = "";
-        document.getElementById("companyname").value = "";
-        document.getElementById("jobtitle").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("phonenumber").value = "";
-        document.getElementById("message").value = ""
-        console.log(res);
-        alert("Message Sent Successfully")
-    }).catch(err => console.log(err));
+    if(params.firstname && params.lastname && params.companyname && params.jobtitle && params.email && params.phonenumber && params.message){
+        const serviceID = "service_2k39r8u";
+        const templateID = "template_us8fs3m"
+        emailjs.send(serviceID,templateID,params).then(function (res){
+            document.getElementById('firstname').value = "";
+            document.getElementById("lastname").value = "";
+            document.getElementById("companyname").value = "";
+            document.getElementById("jobtitle").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("phonenumber").value = "";
+            document.getElementById("message").value = ""
+            console.log(res);
+            alert("Message Sent Successfully")
+        }).catch(err => console.log(err));
+    }
+    else{
+        alert("required field can't be empty");
+    }
 }
 
